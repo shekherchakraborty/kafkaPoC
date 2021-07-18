5dabcdc87bacc6f881f30c57c849a08aa732fb09
shekherchakraborty <shekherchakraborty762@gmail.com>
2021-07-18 21:56:08 +0530

    Initial commit

---
+++ src/main/java/com/ssc/kafka/PocKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.ssc.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssc/kafka/PocKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssc.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
